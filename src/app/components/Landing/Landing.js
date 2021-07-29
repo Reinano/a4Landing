89d6b304df6793ee48d85cbbfa67c9595aa77d93
slavotyk <gamelinkstv@gmail.com>
2021-07-29 23:34:23 +0300

    Roadmap_desktop

--- src/app/components/Landing/Landing.js
+++ src/app/components/Landing/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from "../Footer/Footer";
 import SocialMedia from "../SocialMedia/SocialMedia";
+import Roadmap from "../Roadmap/Roadmap";
 import Navbar from "../Navbar/Navbar";
 
 
@@ -8,6 +9,7 @@ const Landing = () => {
     return (
         <>
             <Navbar/>
+            <Roadmap/>
             <SocialMedia/>
             <Footer/>
         </>
