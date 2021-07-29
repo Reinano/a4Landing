de888f83130d003069c758dd063150b15c9b17ec
vaceslavdacenko <v.dyachenko@offsidegaming.com>
2021-07-29 18:17:46 +0300

    fixes + components

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import './styles/App.css';
+import Landing from "./components/Landing/Landing";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Switch>
-          <Route exact path='/'/>
+            <Route exact path='/' component={Landing}/>
         </Switch>
       </BrowserRouter>
     </>
