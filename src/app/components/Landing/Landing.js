dca537af3f57b9869b86892c3db8d9ac609eb158
Reinano <rtoktogonov@gmail.com>
2021-07-29 21:43:18 +0300

    add navbar component in landing

--- src/app/components/Landing/Landing.js
+++ src/app/components/Landing/Landing.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Footer from "../Footer/Footer";
 import SocialMedia from "../SocialMedia/SocialMedia";
+import Navbar from "../Navbar/Navbar";
+
 
 const Landing = () => {
     return (
         <>
+            <Navbar/>
             <SocialMedia/>
             <Footer/>
         </>
