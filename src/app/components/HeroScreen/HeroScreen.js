3be14272275cc7cd870353af9ed27fe5f5d71806
Reinano <rtoktogonov@gmail.com>
2021-07-30 19:55:56 +0300

    About component Fixed styles in Landing, HeroScreen, CountDown

--- src/app/components/HeroScreen/HeroScreen.js
+++ src/app/components/HeroScreen/HeroScreen.js
@@ -5,23 +5,27 @@ const HeroScreen = () => {
 
 
   return(
-    <div className="container">
-      <section className="heroscreen">
-        <h1 className="heroscreen__header">
-          Binance Smart Chain Leveraged Farming Made Easy With <span className='heroscreen__header_span'>A4 Tokens</span>
-        </h1>
-        <div className="heroscreen__description">
-          Increase Your Assets In Both Bull And Bear Markets. And also get strong leverage without the risk of losing capital.
-        </div>
-        <div className="heroscreenStore">
-          <div className="heroscreenStore__appStore"></div>
-          <div className="heroscreenStore__googlePlay"></div>
-        </div>
+    <div className="heroscreen__background">
+      <div className="container">
+        <section className="heroscreen">
+          <h1 className="heroscreen__header">
+            Binance Smart Chain Leveraged Farming Made Easy With <span className='heroscreen__header_span'>A4 Tokens</span>
+          </h1>
+          <div className="heroscreen__description">
+            Increase Your Assets In Both Bull And Bear Markets. And also get strong leverage without the risk of losing capital.
+          </div>
+          <div className="heroscreenStore">
+            <div className="heroscreenStore__appStore"></div>
+            <div className="heroscreenStore__googlePlay"></div>
+          </div>
 
-        <div className="heroscreen__imgItem">
+          <div className="heroscreen__background_img">
 
-        </div>
-      </section>
+          </div>
+
+          <div className="heroscreen__background_green"></div>
+        </section>
+      </div>
     </div>
   );
 }
