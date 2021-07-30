86ca60389e59433d0b07d9fc46560c2c15bb332f
Reinano <rtoktogonov@gmail.com>
2021-07-30 12:03:10 +0300

    HeroScreen

---
+++ src/app/components/HeroScreen/HeroScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const HeroScreen = () => {
+
+
+
+  return(
+    <div className="container">
+      <section className="heroscreen">
+        <h1 className="heroscreen__header">
+          Binance Smart Chain Leveraged Farming Made Easy With <span className='heroscreen__header_span'>A4 Tokens</span>
+        </h1>
+        <div className="heroscreen__description">
+          Increase Your Assets In Both Bull And Bear Markets. And also get strong leverage without the risk of losing capital.
+        </div>
+        <div className="heroscreenStore">
+          <div className="heroscreenStore__appStore"></div>
+          <div className="heroscreenStore__googlePlay"></div>
+        </div>
+
+        <div className="heroscreen__imgItem">
+
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default HeroScreen;
--- src/app/components/Landing/Landing.js
+++ src/app/components/Landing/Landing.js
@@ -3,12 +3,13 @@ import Footer from "../Footer/Footer";
 import SocialMedia from "../SocialMedia/SocialMedia";
 import Roadmap from "../Roadmap/Roadmap";
 import Navbar from "../Navbar/Navbar";
-
+import HeroScreen from "../HeroScreen/HeroScreen";
 
 const Landing = () => {
     return (
         <>
             <Navbar/>
+            <HeroScreen/>
             <Roadmap/>
             <SocialMedia/>
             <Footer/>
